5b1dc4a5bf9e5be3778d8901f9d7722cd6aab366
Егор Попов <shadermx@yandex.ru>
2024-03-21 19:48:03 +0300

    Improvements 0.2

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import re
+import socket
+from collections import OrderedDict
 
 import requests
 
 
 PHONE_NUMBER_RETRIVE = re.compile(
-    r"(?:(?:8|\+7) {0,1})?(?:\(?\d{3}\)?[\- ]{0,1})[\d\- ]{7,10}"
+    r"class=\"phone-number\".+>([\d\- ()+]+)<"
 )
-# Допускаем что вокруг скобок могут быть пробелы как в примере:
+
 PHONE_NUMBER_CHECK = re.compile(
-    r"^\+?(\d{0,3})( ?\(\d{3,5}\) ?)(\d{1,3})\-(\d{1,2})\-(\d{1,2})$"
+    r"^(\+(\d{0,3}))?(\(\d{3,5}\))(\d{1,3})\-(\d{1,2})\-(\d{1,2})$"
 )
+
+
+TRANSFORM_PATTERNS = OrderedDict({
+    r" ": r"",
+    r"^\(": r"+7(",
+    r"^8\(": r"+7(",
+})
 
 
 def get_response(url: str) -> requests.Response | None:
@@ -23,40 +32,39 @@ def get_response(url: str) -> requests.Response | None:
     return None
 
 
-def get_phone_numbers(data: str) -> str:
+def get_phone_number(data: str) -> str:
     """Получение номера телефона."""
     result = PHONE_NUMBER_RETRIVE.findall(data)
-    print(result)
-    return result
+    return result[0]
 
 
-def valid_phone_number(data: str) -> bool:
+def valid_phone_number(data: str) -> str:
     """Проверка номера телефона на соответствие паттерну."""
-    if PHONE_NUMBER_CHECK.match(data):
-        return True
-    return False
+    for pattern, replacement in TRANSFORM_PATTERNS.items():
+        data = re.sub(pattern, replacement, data)
+    if not PHONE_NUMBER_CHECK.match(data):
+        return "Retrived phone number is not valid."
+    return f"Valid phone number is: {data}"
 
 
-def find_phone_numbers_on_page(url: str) -> list | None:
+def find_phone_numbers_on_page(url: str) -> str:
     """Поиск номеров телефонов на странице."""
-    response = get_response(url)
+    try:
+        ip_address = socket.gethostbyname(url)
+    except socket.gaierror:
+        return f"Host `{url}` is not resolved."
+    print(f"Host `{url}` is at `{ip_address}`.")
+    response = get_response('https://' + url)
     if not response:
-        print(f"Requested page `{url}` is not available.")
-        return None
-    print(f"Requested page `{url}` is available.")
-    phone_nums = get_phone_numbers(response.text)
-    if not phone_nums:
-        print("Phone numbers not found in requested page.")
-        return None
-    valid_numbers = []
-    for num in phone_nums:
-        print(f"Retrived number: {num}")
-        if valid_phone_number(num) and num not in valid_numbers:
-            valid_numbers.append(num)
-    return valid_numbers
+        return f"Requested page `https://{url}` is not available."
+    print(f"Requested page `https://{url}` is available.")
+    phone_num = get_phone_number(response.text)
+    if not phone_num:
+        return "Phone number not found in requested page."
+    print(f"Retrived phone number `{phone_num}`.")
+    return valid_phone_number(phone_num)
 
 
 if __name__ == "__main__":
-    url = "https://sstmk.ru"
-    result = find_phone_numbers_on_page(url)
-    print(f"Found valid phone numbers on page: {result if result else 'None'}")
+    url = "sstmk.ru"
+    print(find_phone_numbers_on_page(url))
